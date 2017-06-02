f0bbaca39b8f02a234044a7567bcf05a6af5e239
martileos <martinevarez@gmail.com>
2017-06-02 14:55:51 -0600

    Buscar usuario y altas

--- abc/js/eventos.js
+++ abc/js/eventos.js
@@ -18,6 +18,8 @@ var iniciaApp = function(){
 			if(data.respuesta==true){
 				$("#datosUsuario").hide();
 				$("nav").show("slow");
+				$("#secUsuarios").show("slow");
+				
 			}else{
 				alert("Usuario no válido");
 			}
@@ -36,12 +38,85 @@ var iniciaApp = function(){
 			entrar();
 		}
 	}
+	var datosUsuario=function(){
+		var usuario=$("#txtNomUsuario").val();
+		var parametros="opcion=datosusuario"+
+					   "&usuario="+usuario+
+					   "&id="+Math.random();
+		var du=$.ajax({
+			method:"POST",
+			url:"php/datos.php",
+			data:parametros,
+			dataType:"json"
+		});
+		du.done(function(data){
+			if(data.respuesta==true){
+				$("#txtNomNombre").val(data.nombre);
+				$("#txtNomClave").val(data.clave);
+				$("#txtNomDepto").val(data.departamento);
+				$("#txtNomVigencia").val(data.vigencia);
+			}else{
+				$("#txtNomNombre").focus();
+			}
+		});
+		du.fail(function(jqError,textStatus){
+
+		});
+	}
+	var teclaNomUsuario=function(tecla){
+		if(tecla.which==13){
+			datosUsuario();
+		}
+	}
+	var altas=function(){
+		var usuario=$("#txtNomUsuario").val(); 
+		var nombre=$("#txtNomNombre").val();
+		var clave=$("#txtNomClave").val();
+		var depto=$("#txtNomDepto").val();
+		var vigencia=$("#txtNomVigencia").val();
+		var parametros="opcion=alta"+
+					   "&usuario="+usuario+
+					   "&nombre="+nombre+
+					   "&clave="+clave+
+					   "&departamento="+depto+
+					   "&vigencia="+vigencia+ 	
+					   "&id="+Math.random();
+		var altaUsuario=$.ajax({
+			method:"POST",
+			url:"php/datos.php",
+			data:parametros,
+			dataType:"json"
+		});
+		altaUsuario.done(function(data){
+			if(data.respuesta==true){
+				alert("Usuario dado de alta");				
+			}else{
+				alert("Usuario existente o no se pudo registrar");
+			}
+		});
+		altaUsuario.fail(function(jqError,textStatus){
+
+		});
+	}
 	//Sección de declaración de eventos
 	$("#btnEntrar").on("click",entrar);
 	$("#txtUsuario").on("keypress",teclaUsuario);
 	$("#txtClave").on("keypress",teclaClave);
+	$("#txtNomUsuario").on("keypress",teclaNomUsuario);
+	$("#btnAltas").on("click",altas);
+
 }
 $(document).ready(iniciaApp);
+
+
+
+
+
+
+
+
+
+
 
 
 
--- abc/php/datos.php
+++ abc/php/datos.php
@@ -16,14 +16,77 @@
 		$salidaJSON = array('respuesta' => $respuesta);
 		print(json_encode($salidaJSON));
 	}
+	function datosUsuario(){
+		$respuesta=false;
+		$usuario=GetSQLValueString($_POST["usuario"],"text");
+		$conexion=conecta();
+		$consulta=sprintf("select * from usuarios where usuario=%s limit 1",$usuario);
+		$resultado=mysql_query($consulta);
+		$nombre="";
+		$clave="";
+		$departamento=0;
+		$vigencia=0;
+		if(mysql_num_rows($resultado)>0){
+			$respuesta=true;
+			if($registro=mysql_fetch_array($resultado)){
+				$nombre       = $registro["nombre"];
+				$clave        = $registro["clave"];
+				$departamento = $registro["departamento"];
+				$vigencia     = $registro["vigencia"];
+			}
+		}
+		$salidaJSON = array('respuesta'   => $respuesta, 
+							'nombre'      => $nombre,
+							'clave'       => $clave,
+							'departamento'=> $departamento,
+							'vigencia'    => $vigencia);
+		print json_encode($salidaJSON);
+	}
+	function alta(){
+		$respuesta=false;
+		$conexion=conecta();
+		$u=GetSQLValueString($_POST["usuario"],"text");
+		$n=GetSQLValueString($_POST["nombre"],"text");
+		$c=GetSQLValueString(md5($_POST["clave"]),"text");
+		$d=GetSQLValueString($_POST["departamento"],"int");
+		$v=GetSQLValueString($_POST["vigencia"],"int");
+		//Buscar si existe
+		$busca=sprintf("select usuario from usuarios where usuario=%s",$u);
+		$resultadoBusca=mysql_query($busca);
+		if(mysql_num_rows($resultadoBusca)==0){//Si no existe
+			$inserta=sprintf("insert into usuarios values(default,%s,%s,%s,%d,%d)",$u,$n,$c,$d,$v);
+			mysql_query($inserta);
+			if(mysql_affected_rows()>0){ 
+				$respuesta=true;
+			}	
+		}
+		$salidaJSON = array('respuesta' => $respuesta );
+		print json_encode($salidaJSON);
+	}
 	//Menú principal
 	$opcion=$_POST["opcion"];
 	switch ($opcion) {
 		case 'valida':
 			valida();
-			break;		
+			break;
+		case 'datosusuario':
+			datosUsuario();
+			break;	
+		case 'alta':
+			alta();
+			break;	
 		default:
 			# code...
 			break;
 	}
 ?>
+
+
+
+
+
+
+
+
+
+
